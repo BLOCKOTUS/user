afc883a4d0d2ac2fb7b387f750007892ab620824
Daniel Febrero <febrero@me.com>
2021-01-25 18:03:10 +0100

    BLOCKOTUS/organism#112 remove keypair contract

--- chaincode/javascript/index.js
+++ chaincode/javascript/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const User = require('./lib/user.js');
-const Keypair = require('./lib/keypair.js');
 
 module.exports.User = User;
-module.exports.Keypair = Keypair;
-module.exports.contracts = [ User, Keypair ];
+module.exports.contracts = [ User ];
--- chaincode/typescript/index.ts
+++ chaincode/typescript/index.ts
@@ -1,8 +1,6 @@
 'use strict';
 
-import { Keypair } from './lib/keypair';
 import { User } from './lib/user';
-export { Keypair } from './lib/keypair';
 export { User } from './lib/user';
 
-export const contracts: any[] = [ User, Keypair ];
+export const contracts: any[] = [ User ];
